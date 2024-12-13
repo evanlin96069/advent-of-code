c9a6f0c230476c1034c70920527c28c082541013
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-12-13 00:26:20 -0500

    Add day13

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,13 +1,34 @@
-with open("sample.txt") as f:
-    f.read().split('\n')
+import re
+
+
+data = []
+
+with open("input.txt") as f:
+    for block in f.read().split('\n\n'):
+        pattern = r'X[+=](\d+), Y[+=](\d+)'
+        matches = re.findall(pattern, block)
+        data.append([tuple(map(int, match)) for match in matches])
+
+def solve(a, b, p):
+    det = a[0] * b[1] - b[0] * a[1]
+    dx = p[0] * b[1] - b[0] * p[1]
+    dy = a[0] * p[1] - p[0] * a[1]
+
+    x = dx / det
+    y = dy / det
+
+    if x.is_integer() and y.is_integer():
+        return int(x * 3 + y * 1)
+
+    return 0
 
 
 def part1():
-    pass
+    print(sum(solve(a, b, p) for a, b, p in data))
 
 
 def part2():
-    pass
+    print(sum(solve(a, b, (p[0]+10000000000000, p[1]+10000000000000)) for a, b, p in data))
 
 
 part1()
