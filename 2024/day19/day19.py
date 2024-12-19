6a329240614f6b81122b4d10aa7e90d338438ffb
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-12-19 15:44:46 +0800

    Add day19

--- 2024/day19/day19.py
+++ 2024/day19/day19.py
@@ -1,13 +1,39 @@
-with open("sample.txt") as f:
-    f.read().split('\n')
+from functools import lru_cache
 
+
+patterns = None
+data = None
+
+with open("input.txt") as f:
+    t1, t2 = f.read().split('\n\n')
+    patterns = t1.split(', ')
+    data = t2.split('\n')
 
 def part1():
-    pass
+    def search(s):
+        if len(s) == 0:
+            return True
+
+        for pattern in patterns:
+            if (s.startswith(pattern) and
+                search(s[len(pattern):])):
+                    return True
+
+        return False
+
+    print(sum([search(s) for s in data]))
 
 
 def part2():
-    pass
+    @lru_cache
+    def search(s):
+        if len(s) == 0:
+            return 1
+
+        return sum([search(s[len(pattern):]) if s.startswith(pattern) else 0 for pattern in patterns])
+
+
+    print(sum([search(s) for s in data]))
 
 
 part1()
