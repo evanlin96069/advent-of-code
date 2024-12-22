87e90d38b4bba58ce6e2ca3c3d102b1c7bcbcc7d
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-12-22 21:44:39 +0800

    Add day22

--- 2024/day22/day22.py
+++ 2024/day22/day22.py
@@ -1,13 +1,51 @@
-with open("sample.txt") as f:
-    f.read().split('\n')
+from collections import defaultdict, deque
+
+
+data = None
+
+with open("input.txt") as f:
+    data = list(map(int, f.read().split('\n')))
+
+def next(n):
+    n = ((n << 6) ^ n) & 0xFFFFFF
+    n = ((n >> 5) ^ n) & 0xFFFFFF
+    n = ((n << 11) ^ n) & 0xFFFFFF
+    return n
 
 
 def part1():
-    pass
+    count = 0
+    for n in data:
+        for _ in range(2000):
+            n = next(n)
+        count += n
+
+    print(count)
 
 
 def part2():
-    pass
+    total = defaultdict(int)
+    for n in data:
+        seen = set()
+        changes = deque(maxlen=4)
+        prev = n % 10
+        for _ in range(3):
+            n = next(n)
+            price = n % 10
+            changes.append(price - prev)
+            prev = price
+
+        for _ in range(1997):
+            n = next(n)
+            price = n % 10
+            changes.append(price - prev)
+            seq = tuple(changes)
+            if seq not in seen:
+                total[tuple(changes)] += price
+                seen.add(seq)
+            prev = price
+
+    print(max(total.values()))
 
 
 part1()
