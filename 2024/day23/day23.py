e6a0ad63e6b0c1f7f1b8da86dde050d1e01fcbbb
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-12-23 15:02:31 +0800

    Add day23

--- 2024/day23/day23.py
+++ 2024/day23/day23.py
@@ -1,13 +1,17 @@
-with open("sample.txt") as f:
-    f.read().split('\n')
+import networkx as nx
 
+cliques = None
+
+with open("input.txt") as f:
+    G = nx.Graph([l.split('-') for l in f.read().split('\n')])
+    cliques = list(nx.enumerate_all_cliques(G))
 
 def part1():
-    pass
+    print(sum(any(a.startswith("t") for a in c) for c in cliques if len(c) == 3))
 
 
 def part2():
-    pass
+    print(",".join(sorted(max(cliques, key=len))))
 
 
 part1()
