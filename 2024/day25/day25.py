d491a62d75d901d866af387d2b4faf25543000dd
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-12-25 17:06:26 +0800

    Add day25; 2024 Done

--- 2024/day25/day25.py
+++ 2024/day25/day25.py
@@ -1,9 +1,19 @@
-with open("sample.txt") as f:
-    f.read().split('\n')
+locks = []
+keys = []
 
+with open("input.txt") as f:
+    for s in [s.split('\n') for s in f.read().split('\n\n')]:
+        c = s[0][0]
+        result = []
+        for i in range(5):
+            result.append(sum([row[i] == '#' for row in s]))
+        if c == '#':
+            locks.append(result)
+        else:
+            keys.append(result)
 
 def part1():
-    pass
+    print(sum(all(k+l <= 7 for k, l in zip(key, lock)) for key in keys for lock in locks))
 
 
 def part2():
